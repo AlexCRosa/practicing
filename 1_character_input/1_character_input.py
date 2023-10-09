75511124b557f526f0e66359a8ff5f8fe3e2a032
Alex Cesar <alex_cesar20@hotmail.com>
2023-10-09 07:48:03 -0400

    Add exercise

---
+++ 1_character_input/1_character_input.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+year = (2023 - age) + 100
+
+print(f"{name} you will be 100 years old in the year of {year}.")
+
+number = int(input("Give me a number: "))
+
+for i in range(number):
+    print(f"{name} you will be 100 years old in the year of {year}.\n")
