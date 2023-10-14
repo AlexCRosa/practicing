6a9affa671a34b7334315e207e35a555556f0166
Alex Cesar <alex_cesar20@hotmail.com>
2023-10-13 21:11:22 -0400

    Add exercise 4

--- 4_divisors/4_divisors.py
+++ 4_divisors/4_divisors.py
@@ -1,11 +1,14 @@
 # Prompt the user for a number
 num = int(input("Tell me a number: "))
 
-x = range(0, num)
-
 # Create a list of all divisors of the given number
 new_list = []
 
-# Check the divisors
-for i in x:
-    if i % 
+# Loop through numbers from 1 to the given number
+for i in range(1, num + 1):
+
+    # Check the divisors
+    if num % i == 0:
+        new_list.append(i) 
+
+print(new_list)
