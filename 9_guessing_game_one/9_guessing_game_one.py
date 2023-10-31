b421880613bd067a9be535eb2c6e8b09ac897a3a
Alex Cesar <alex_cesar20@hotmail.com>
2023-10-31 16:12:31 -0400

    Add exercise 9

---
+++ 9_guessing_game_one/9_guessing_game_one.py
@@ -0,0 +1,27 @@
+import random
+
+generate_number = random.randint(1, 10)
+attempts = 0
+
+while True:
+    user_input = input("Guess a number between 1 and 10? ")
+
+    if user_input.lower() == "exit":
+        print(f"You made {attempts} attempts.")
+        print("See you later!")
+        break
+
+    try:
+        user_guess = int(user_input)
+        attempts += 1            
+
+        if user_guess > generate_number:
+            print("That's HIGH! Guess another number: ")
+        elif user_guess < generate_number:
+            print("That's LOW! Guess another number: ")
+        else:
+            print(f"Well done! The number is {generate_number}.")
+            generate_number = random.randint(1, 10)
+            attempts += 1
+    except ValueError:
+        print("Please enter a valid number or type 'exit'.")
