864c9f26722b136773c4f943289312167ce6bba2
Alex Cesar <alex_cesar20@hotmail.com>
2023-11-14 16:15:38 -0500

    Add exercise 13

--- 13_fibonacci/fibonacci.py
+++ 13_fibonacci/fibonacci.py
@@ -1,15 +1,21 @@
-from operator import length_hint 
+def fibonacci(x):
+
+    # Defining a list to receive values
+    fibonacci_list = [1]
+
+    # Defining a counter to help with the calculation
+    counter = 1
+
+    # Loop until the list reaches the amount of elements the user wanted
+    while len(fibonacci_list) < x:
+        fibonacci_list.append(counter)
+
+        # Fibonacci logic = sum of the two last values
+        counter = fibonacci_list[-1] + fibonacci_list[-2]
+    return fibonacci_list
 
 amount = int(input("How many numbers to generate? "))
 
-def fibonacci(x):
-    fibonacci_sequence = []
-    start_number = 1
-    while fibonacci_sequence.len() < x:
-        fibonacci_sequence.append(start_number)
-        start_number += start_number
-        return fibonacci_sequence
+a = fibonacci(amount)
 
-fibonacci_sequence = fibonacci(amount)
-
-print(fibonacci_sequence)
+print(a)
