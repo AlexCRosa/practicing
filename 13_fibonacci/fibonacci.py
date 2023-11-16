5a1e6333b0b71f90c6a4f5226c5be74fc0419463
Alex Cesar <alex_cesar20@hotmail.com>
2023-11-16 15:02:50 -0500

    Add exercise 13

--- 13_fibonacci/fibonacci.py
+++ 13_fibonacci/fibonacci.py
@@ -1,29 +1,24 @@
 def fibonacci(x):
 
-    # Defining a list to receive values
+    # Defining a counter to manage user's input
     counter = 0
-    
-    # Creating a variable just to store a value
-    joker = 0
 
-    # Defining a counter to help with the calculation 
-    counter1 = 0
-    counter2 = 1
+    # Defining variables to handle the logic of Fibonacci
+    num1 = 0
+    num2 = 1
+    fibonacci_number = num1 + num2
 
-    # Loop until the list reaches the amount of elements the user wanted
+    # Loop until the list reaches the amount of numbers the user wanted
     while counter < x:
+        print(fibonacci_number, end=" ")
+
+        # Fibonacci logic = sum of the two previous values
+        fibonacci_number = num1 + num2
+        num1 = num2
+        num2 = fibonacci_number
+
         counter += 1
-        print(counter2, end=" ")
-
-        # Fibonacci logic = sum of the two last values
-        joker = counter1
-
-        counter2 = counter1 + counter
-
-    return fibonacci_list
 
 amount_numbers = int(input("How many numbers to generate? "))
 
-a = fibonacci(amount_numbers)
-
-print(a)
+fibonacci(amount_numbers)
