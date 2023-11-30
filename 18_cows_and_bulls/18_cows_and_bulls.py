9d3c9e4f27d6c91840d5d85354f069fa30c9280a
Alex Cesar <alex_cesar20@hotmail.com>
2023-11-30 09:50:16 -0500

    Add exercise 18

---
+++ 18_cows_and_bulls/18_cows_and_bulls.py
@@ -0,0 +1,22 @@
+import random
+
+# Create and store a random number into a list
+random_number = random.randint(1000, 9999)
+print(random_number)
+
+generated_number = [int(i) for i in str(random_number)]
+print(generated_number)
+
+
+# Managing user's guess inside a while loop
+
+    # Ask user for guess a number
+
+
+    # Store user's guess into a list
+
+
+    # Compare the generated number and user's guess
+
+
+    # Print a message based on the result
